3d9188dfa93babef62a95a65cdb0612d76385bd0
William Zheng <official.william.zheng@gmail.com>
2025-07-21 23:37:54 -0400

    sidebar

--- game-gazer-pro/src/components/SideBar.jsx
+++ game-gazer-pro/src/components/SideBar.jsx
@@ -1,9 +1,21 @@
+import { Link } from "react-router-dom";
 
-const SideBar = () =>{
-    return(
-        <div>
+const SideBar = () => {
+  return (
+    <nav className="sidebar-nav">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/">Search</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-        </div>
-    )
-}
-export default SideBar
+export default SideBar;
